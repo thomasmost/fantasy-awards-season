eecabf2616eab008be508c4bf5d6701dcafe510d
Thomas Moore <thomas@kava.io>
2021-12-09 03:31:09 -0500

    add loading

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,10 @@ const Caption = styled.div`
   margin-bottom: 50px;
 `;
 
+const Svg = styled.svg`
+  margin-top: 100px;
+`;
+
 const getRandFromRng = (rng: string[]) => {
   const optionCount = rng.length;
   return rng[Math.floor(Math.random() * optionCount)];
@@ -62,6 +66,85 @@ function App() {
           <h1>Fantasy Film Awards, 2021</h1>
         </Header>
       </div>
+      {loading && (
+        <Svg
+          width="400"
+          height="400"
+          viewBox="0 0 45 45"
+          xmlns="http://www.w3.org/2000/svg"
+          stroke="#86c6ee"
+        >
+          <g
+            fill="none"
+            fill-rule="evenodd"
+            transform="translate(1 1)"
+            stroke-width="2"
+          >
+            <circle cx="22" cy="22" r="6" stroke-opacity="0">
+              <animate
+                attributeName="r"
+                begin="1.5s"
+                dur="3s"
+                values="6;22"
+                calcMode="linear"
+                repeatCount="indefinite"
+              />
+              <animate
+                attributeName="stroke-opacity"
+                begin="1.5s"
+                dur="3s"
+                values="1;0"
+                calcMode="linear"
+                repeatCount="indefinite"
+              />
+              <animate
+                attributeName="stroke-width"
+                begin="1.5s"
+                dur="3s"
+                values="2;0"
+                calcMode="linear"
+                repeatCount="indefinite"
+              />
+            </circle>
+            <circle cx="22" cy="22" r="6" stroke-opacity="0">
+              <animate
+                attributeName="r"
+                begin="3s"
+                dur="3s"
+                values="6;22"
+                calcMode="linear"
+                repeatCount="indefinite"
+              />
+              <animate
+                attributeName="stroke-opacity"
+                begin="3s"
+                dur="3s"
+                values="1;0"
+                calcMode="linear"
+                repeatCount="indefinite"
+              />
+              <animate
+                attributeName="stroke-width"
+                begin="3s"
+                dur="3s"
+                values="2;0"
+                calcMode="linear"
+                repeatCount="indefinite"
+              />
+            </circle>
+            <circle cx="22" cy="22" r="8">
+              <animate
+                attributeName="r"
+                begin="0s"
+                dur="1.5s"
+                values="6;1;2;3;4;5;6"
+                calcMode="linear"
+                repeatCount="indefinite"
+              />
+            </circle>
+          </g>
+        </Svg>
+      )}
       {!loading && (
         <>
           <Container>
